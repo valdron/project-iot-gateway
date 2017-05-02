d3a3117f1ddb5850f78b98cfa537b611f2b5d5d8
valdron <seehofer.paul@gmail.com>
2017-05-02 14:59:00 +0200

    configuration.h modified

--- src/configuration.h
+++ src/configuration.h
@@ -2,13 +2,19 @@
 //TODO: Functions for reading from file and creating config structs
 
 
-//
+//TODO: Design config structs
 
-typedef struct IG_ConfigRequest{
-    IG_Datatype input,
-    IG_Datatype output
-    void * data
-} IG_ConfigRequest;
+// typedef struct IG_Config{
+//     /*...*/
+// } IG_Config;
 
+// typedef struct IG_ConfigRequest{
+//     /*...*/
+// } IG_ConfigRequest;
 
-IG
+// typedef struct IG_ConfigResponse{
+//     /*...*/
+// } IG_ConfigResponse;
+
+// generic config request function
+void get_config(IG_Config * config, IG_ConfigRequest * request, IG_ConfigResponse * response);
