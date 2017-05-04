b8d4fd68ff5f2d0df4fee49cb3b84b9357a41313
Paul Seehofer <seehofer.paul@gmail.com>
2017-05-04 20:13:01 +0200

    modified configuration.h

--- src/configuration.h
+++ src/configuration.h
@@ -8,16 +8,34 @@
 //TODO: Design config structs
 
 typedef struct{
+    IG_ConfigType type;
+    // xmlDoc ptr
     /*...*/
 } IG_Config;
 
 typedef struct{
     /*...*/
+    IG_ConfigRequestDataType requestType;
+    IG_ConfigResponseDataType requestedResponsType;
+    void * data;
 } IG_ConfigRequest;
 
 typedef struct{
+    IG_Bool successful;
+    IG_ConfigResponseDataType responseType;
+    IG_UInt32 responseAmount;
+    void * data;
     /*...*/
 } IG_ConfigResponse;
+
+//describes what kind of Config it is
+typedef enum {STANDARD, VERARBEITER, OPC, MQTT } IG_ConfigType;
+
+//Describes what is in the dataptr of the ConfigRequest
+typedef enum { UA_NODEID, IG_ID, NONE } IG_ConfigRequestDataType;
+
+//Describes what kind of information is expected as a Result in the ConfigResponse and what really is in the ConfigResponse
+typedef enum { IG_ID, MQTT_TOPIC, OPC_SUBSCRIPTIONS, VERARBEITUNG, OPC_CONFIG, MQTT_CONFIG, VERARBEITER_CONFIG } IG_ConfigResponseDataType;
 
 // generic config request function
 void get_config(IG_Config * config, IG_ConfigRequest * request, IG_ConfigResponse * response);
