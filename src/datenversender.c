84bb22f8c1e680133aec1c1ab11dfe82fe380e45
umadbro96 <dilait01@hs-esslingen.de>
2017-05-07 09:46:00 +0200

    implement create/delete ... thread waiting loop inc. soon!!!!

--- src/datenversender.c
+++ src/datenversender.c
@@ -1,1 +1,14 @@
 #include "datenversender.h"
+
+IG_Datenversender * IG_Datenversender_create(IG_Config * config) {
+    IG_Datenversender * versender = (IG_Datenversender *) malloc(sizeof(IG_Datenversender));
+    versender->config = config;
+    versender->queue = IG_Queue_new();
+    return versender;
+}
+
+
+void IG_Datenversender_delete(IG_Datenversender * versender) {
+    IG_Queue_delete(versender->queue);
+    free(versender);
+}
