a2fa882855ebe21f6d0b95d5cc1c132d2f1db09e
valdron <seehofer.paul@gmail.com>
2017-05-08 01:06:26 +0200

    queue now has 2 types (blocking and non-blocking) \nmoved queuetests to its own dir\ncreated new queuetest for blocking queue\nfixed some CMakeLists errors

--- src/datenerfasser.c
+++ src/datenerfasser.c
@@ -4,7 +4,7 @@
 IG_Datenerfasser * IG_Datenerfasser_create(IG_Config * config) {
     IG_Datenerfasser * erfasser = (IG_Datenerfasser *) malloc(sizeof(IG_Datenerfasser));
     erfasser->config = config;
-    erfasser->queue = IG_Queue_new();
+    erfasser->queue = IG_Queue_new(IG_QUEUE_NONBLOCKING);
     return erfasser;
 }
 
--- src/queue.c
+++ src/queue.c
@@ -1,31 +1,37 @@
 #include "queue.h"
-#include "internals.h"
 
-IG_Queue * IG_Queue_new(){
+IG_Queue * IG_Queue_new(IG_QueueType type){
 
 	// Init queue
 	IG_Queue* queue = (IG_Queue*)malloc(sizeof(IG_Queue));
 	queue->size = 0;
 	queue->front = NULL;
 	queue->end = NULL;
-	queue->mutex = malloc(sizeof(pthread_mutex_t));
+	queue->type = type;
 
 	// Init mutex for this specific queue
-	pthread_mutex_init(queue->mutex,NULL);
+	pthread_mutex_init(&queue->mutex,NULL);
+	//only init sem on blocking queue
+	if(queue->type == IG_QUEUE_BLOCKING)
+		//init the semaphore with start 0 and for sharing between threads(second zero)
+		sem_init(&queue->size_sem, 0, 0);
 	return queue;
 }
 
 void IG_Queue_delete(IG_Queue * queue){
-	pthread_mutex_lock(queue->mutex);
+	pthread_mutex_lock(&queue->mutex);
 	while(!IG_Queue_isEmpty(queue)){
 		IG_Data* element = IG_Queue_take(queue);
 		free(element->data);		
 		free(element);
 		//IG_Data_delete(element);
 	}
-	pthread_mutex_unlock(queue->mutex);
-	pthread_mutex_destroy(queue->mutex);
-	free(queue->mutex);
+	if(queue->type == IG_QUEUE_BLOCKING)
+		sem_destroy(&queue->size_sem);
+
+	pthread_mutex_unlock(&queue->mutex);
+	pthread_mutex_destroy(&queue->mutex);
+	
 	free(queue);
 }
 
@@ -34,7 +40,7 @@ void IG_Queue_put(IG_Queue* queue, IG_Data* new_data){
 
 	// Trying to lock this queue from other threads, if it's already locked this thread will
 	// get locked and will be blocked until the mutex is unlocked by another thread
-	pthread_mutex_lock(queue->mutex);
+	pthread_mutex_lock(&queue->mutex);
 
 	// Creating new element for the queue
 	IG_QElement* element = (IG_QElement*)malloc(sizeof(IG_QElement));
@@ -54,9 +60,10 @@ void IG_Queue_put(IG_Queue* queue, IG_Data* new_data){
 		queue->end = element;
 	}
 
-	++(queue->size);
-	
-	pthread_mutex_unlock(queue->mutex);
+	++(queue->size);	
+	pthread_mutex_unlock(&queue->mutex);
+	if(queue->type == IG_QUEUE_BLOCKING)
+		sem_post(&queue->size_sem);
 }
 
 // Overloading function to ensure data is not lost while beeing on the stack
@@ -72,14 +79,15 @@ void IG_Queue_put_copy(IG_Queue* queue, IG_Data new_data) {
 
 IG_Data* IG_Queue_take(IG_Queue* queue){
 
-
+	if(queue->type == IG_QUEUE_BLOCKING)
+		sem_wait(&queue->size_sem);
 	// Lock
-	pthread_mutex_lock(queue->mutex);
+	pthread_mutex_lock(&queue->mutex);
 
 	// Check if queue is empty???
 	if(queue->size==0){
-		pthread_mutex_unlock(queue->mutex);	
-		return &IG_DATA_EMPTY;
+		pthread_mutex_unlock(&queue->mutex);	
+		return NULL;
 	}
 	
 	// Get pointer to first element in queue
@@ -89,7 +97,7 @@ IG_Data* IG_Queue_take(IG_Queue* queue){
 
 	--(queue->size);
 
-	pthread_mutex_unlock(queue->mutex);	
+	pthread_mutex_unlock(&queue->mutex);	
 
 	//take datapointer out of listelement
 	IG_Data * data = dequeued_data->data;
