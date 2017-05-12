46de726dc2abf626b4af5a6b2a1f986cd7b9efab
ValerianWeiss <valerian@mail.com>
2017-05-12 16:04:24 +0200

    stateNode dem OPC-Server hinzugefügt

--- test/Server_v2/state.c
+++ test/Server_v2/state.c
@@ -1,4 +1,5 @@
-#include "open62541.h"
+#include <open62541.h>
+#include <stdio.h>
 #include "state.h"
 
 // State:
@@ -13,8 +14,6 @@
 // Bit 5: 1 = on; 0 = off
 // Bit 6: 1 = on; 0 = off
 // Bit 7: 1 = on; 0 = off
-
-#define STARTSTATE 0b00000001
 
 UA_DateTimeStruct getCurrentTimestruct() {
     return UA_DateTime_toStruct( UA_DateTime_now());
@@ -51,36 +50,4 @@ UA_Byte getState(UA_DateTimeStruct dt) {
 }
 
 
-static UA_StatusCode readCurrentState(void *handle, const UA_NodeId nodeid, UA_Boolean sourceTimeStamp, 
-                                        const UA_NumericRange *range, UA_DataValue *dataValue) {
-    
-    UA_DateTimeStruct dt = getCurrentTimestruct();
 
-        
-    UA_Byte  val = getState(dt);
-    UA_Variant_setScalarCopy(&dataValue->value, &val,  &UA_TYPES[UA_TYPES_BYTE]);
-    dataValue->hasValue = true;
-    return UA_STATUSCODE_GOOD;
-}
-
-void addCurrentStateSourceVariable(UA_Server *server, int nodeNr) {
-        UA_VariableAttributes attr;
-    UA_VariableAttributes_init(&attr);
-    attr.displayName = UA_LOCALIZEDTEXT("en_US", "Current state - data source");
-
-    UA_NodeId currentNodeId = UA_NODEID_STRING(nodeNr , "current-state-datasource");
-    UA_QualifiedName currentName = UA_QUALIFIEDNAME(nodeNr , "current-state-datasource");
-    UA_NodeId parentNodeId = UA_NODEID_NUMERIC(0, UA_NS0ID_OBJECTSFOLDER);
-    UA_NodeId parentReferenceNodeId = UA_NODEID_NUMERIC(0, UA_NS0ID_ORGANIZES);
-    UA_NodeId variableTypeNodeId = UA_NODEID_NULL;
-
-    UA_DataSource stateDataSource;
-    stateDataSource.handle = NULL;
-    stateDataSource.read = readCurrentState;
-    stateDataSource.write = NULL;
-    UA_Server_addDataSourceVariableNode(server, currentNodeId, parentNodeId,
-                                        parentReferenceNodeId, currentName,
-                                        variableTypeNodeId, attr,
-                                        stateDataSource, NULL);
-
-}
