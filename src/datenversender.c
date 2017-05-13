87e96d1dc692408cd93c3bf61fd753daa7aad13c
Dimitrios <dilait01@hs-esslingen.de>
2017-05-13 07:41:29 +0200

    Callback-Function set with loop for data processing

--- src/datenversender.c
+++ src/datenversender.c
@@ -28,7 +28,7 @@ IG_Status sendData(IG_Datenversender * sender, IG_Data * data) {
 void* doSomeThing(void *arg)
 {
             // Loop, when thread creation is succeeded
-    while (/* Laufbedingung 0*/)
+    while ( 1 )
      {
 
   struct readThreadParams *params = arg;
