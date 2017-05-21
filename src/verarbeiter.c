434b2cbe70efdf49ddbb299e7ccd97e50c7a4420
FiratSusan <firat_susan@hotmail.com>
2017-05-21 23:50:20 +0200

    Updated verarbeitung.h and verarbeitung.c

--- src/verarbeiter.c
+++ src/verarbeiter.c
@@ -14,3 +14,22 @@ IG_Verarbeiter * IG_Verarbeiter_create(IG_Config * config, IG_Datenversender * s
 void IG_Verarbeiter_delete(IG_Verarbeiter * verarbeiter) {
     free(verarbeiter);
 }
+
+IG_Status init_verarbeiter(IG_Verarbeiter * verarbeiter){
+	//Insert magic here for config where rules sets are created
+	
+	//Output of magic is an array of IG_Input_RuleSet	
+	IG_INT32 size= 0;
+	IG_Input_RuleSet ruleSets[size]= NULL;
+	
+	workLoopArgs args = (workLoopArgs){verarbeiter, ruleSets, size};
+	pthread_t thread;	
+
+	pthread_create(&thread,NULL,workLoop,(void*)&args)
+
+	return IG_GOOD;
+}
+
+void* workLoop(void*){
+	
+}
--- src/verarbeiter.h
+++ src/verarbeiter.h
@@ -10,7 +10,37 @@ typedef struct{
     IG_Config * config;
     IG_Datenversender * sender;
     IG_Datenerfasser * erfasser;
+    IG_Bool running;
 } IG_Verarbeiter;
+
+typedef enum{
+	IG_RULE_TRANSMIT,
+	IG_RULE_AVG,
+	IG_RULE_MAX,
+	IG_RULE_MIN
+} IG_RuleType;
+
+typedef struct{
+	IG_RuleType rule;
+	IG_INT32 outputId;
+	IG_DateTime deadline;
+	IG_DateTime interval;
+	IG_Data* data;
+} IG_Rule;
+
+typedef struct{
+	IG_INT32 inputId;
+	IG_Datatype datatype;
+	IG_INT32 ruleSize; //number of rules in this RuleSet
+	IG_Rule* rules;	// pointer to an array of rules	
+} IG_Input_RuleSet;
+
+typedef struct{
+	IG_Verarbeiter* verarbeiter;
+	IG_INT32 ruleSetSize;
+	IG_Input_RuleSet* ruleSets;
+} workLoopArgs;
+
 
 //initializes the struct and starts the thread-loop
 IG_Status init_verarbeiter(IG_Verarbeiter * verarbeiter);
@@ -20,5 +50,7 @@ IG_Verarbeiter * IG_Verarbeiter_create(IG_Config * config, IG_Datenversender * s
 
 // frees the memory needed for the struct
 void IG_Verarbeiter_delete(IG_Verarbeiter * verarbeiter);
+
+void* workLoop(void*);
 
 #endif
