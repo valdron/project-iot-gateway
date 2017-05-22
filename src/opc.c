8132125b65226328d494d2a567867a2f4d1f479d
valdron <seehofer.paul@gmail.com>
2017-05-22 17:16:03 +0200

    bla

--- src/opc.c
+++ src/opc.c
@@ -3,7 +3,10 @@
 #include <open62541.h>
 #include <stdio.h>
 #include "opc.h"
+#include "config_opc.h"
 
+IG_Status init(UA_Client * client, MonitoredItems * monitoredItems, IG_Datenerfasser * param);
+void response_handler(UA_UInt32 monId, UA_DataValue *value, void *context);
 
 //parameter ist vom Typ IG_Datenerfasser
 void start_OPC_Client_thread(void * parameter){
@@ -46,7 +49,7 @@ void start_OPC_Client_thread(void * parameter){
   
   
   //FIXME: Init not ready TO DO 
-  init(client, monitoredItems, param->queue);
+  init(client, monitoredItems, param);
 
   printf("\nInit done!\n\n");
 
@@ -68,3 +71,43 @@ void start_OPC_Client_thread(void * parameter){
   UA_Client_delete(client);
 }
 	
+
+
+  IG_Status init(UA_Client * client, MonitoredItems * monitoredItems, IG_Datenerfasser * param) {
+      IG_ConfigResponse res;
+      IG_Status rt = IG_Config_OPC_get_subscriptions(param->config, &res);
+      if(rt != IG_STATUS_GOOD) {
+        return IG_STATUS_BAD;
+      }
+      IG_UInt32 subAmount = res.responseAmount;
+      IG_OPC_Subscription * subs = res.data;
+
+      for(IG_UInt32 i = 0; i < subAmount; i++ ) {
+
+        IG_ConfigResponse res2;
+        rt = IG_Config_OPC_get_items(param->config, &res2, subs[i].subscription_id);
+        if(rt != IG_STATUS_GOOD) {
+          return IG_STATUS_BAD;
+        }
+        IG_UInt32 itemAmount = res2.responseAmount;
+        IG_OPC_Item * items = res2.data;
+        
+        //needs to be saved elsewhere (in Monitoreditemsstruct )
+        IG_UInt32 subid;
+
+        UA_Client_Subscriptions_new(client, UA_SubscriptionSettings_standard,&subid);
+
+        for(IG_UInt32 i = 0; i < itemAmount; i++ ) {
+          //needs to be stored elsewhere (in Monitored items struct)
+          IG_UInt32 itemid;
+          UA_NodeId id = UA_NODEID_NUMERIC(1,items[i].nodeid);
+                                                                                            //vvv insert pointer to correct Monitored Item here
+          UA_Client_Subscriptions_addMonitoredItem(client, subid, id, 0, &response_handler, (void*) &items[i], &itemid);
+        }
+      }
+  }
+
+
+  void response_handler(UA_UInt32 monId, UA_DataValue *value, void *context) {
+    printf("hello");
+  }
--- test/Client_v2/OPCTestmain.c
+++ test/Client_v2/OPCTestmain.c
@@ -3,11 +3,11 @@
 #include<pthread.h>
 #include<stdio.h>
 
-#include "init_xml.h"
 #include "internals.h"
 #include "datenerfasser.h"
 #include "OPC_Queuetest.h"
 #include "opc.h"
+#include "config_opc.h"
 
 
 int main(void) {
@@ -17,8 +17,9 @@ int main(void) {
     //-> Testweise wird NULL übergeben da config noch nicht implementiert ist  
     pthread_t OPC_Client_Thread;
 
+    IG_Config * config = IG_Config_create("opc_config.xml", IG_CONFIG_OPC);
     //Erstelle duei Queue in die der OPC Cliet schreibt
-    IG_Datenerfasser *erfasser = IG_Datenerfasser_create_nonBlocking(NULL);
+    IG_Datenerfasser *erfasser = IG_Datenerfasser_create_nonBlocking(config);
 
     if(pthread_create(&OPC_Client_Thread, NULL, (void*) start_OPC_Client_thread, erfasser) != 0){
             printf("OPC Client konnte nicht gestartet werden\nProgramm wird beendet");
@@ -32,5 +33,6 @@ int main(void) {
             return -1;
     }
 
+    pthread_join(OPC_Client_Thread,NULL);
     pthread_join(readfromOPCqueue,NULL);
 }
